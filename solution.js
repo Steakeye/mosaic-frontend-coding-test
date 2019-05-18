8ffcc43c626f11f8c66e205b1188520b1ddcb7de
Andrew Keats <steakeye.studios@gmail.com>
2019-05-18 13:50:54 +0100

    wip: deserializing

--- solution.js
+++ solution.js
@@ -2,3 +2,52 @@
 export function add(...numsToAdd) {
     return numsToAdd.reduce((orginal, toAdd) => orginal + toAdd);
 }
+
+function safelyAddDeepValue(objectToAugment, keyPath, value) {
+    let currentContext = objectToAugment;
+    keyPath.forEach((key, idx)=> {
+        let nextContext = currentContext[key];
+
+        if (!nextContext) {
+            const nextKey = keyPath[idx+1];
+
+            if (nextKey !== undefined) {
+                const nextObjectShouldBeArray = typeof nextKey === 'number';
+                nextContext = currentContext[key] = nextObjectShouldBeArray ? [] : {}
+            } else {
+                currentContext[key] = value;
+            }
+        }
+        currentContext = nextContext
+    })
+}
+
+export function deserialize(dataToRestructure) {
+    const keyPattern = /(\w+)(\d)_(\w+)/;
+
+    const restucturesdData = {};
+
+    Object.entries(dataToRestructure).forEach(([key, value]) => {
+        //console.log('log', ...arguments)
+        //console.log('keyPattern', keyPattern);
+        //console.log('key', key);
+        //console.log('key, value', key, value)
+        const keyMatchesPattern = key.match(keyPattern);
+
+        if (keyMatchesPattern) {
+            const keysToCreate = keyMatchesPattern.slice(1);
+
+            keysToCreate[1] = parseInt(keysToCreate[1]);
+
+            //console.log('keysToCreate', keysToCreate);
+            //restucturesdData[]
+            //console.log('typeof keysToCreate[1]', typeof keysToCreate[1], keysToCreate[1])
+            safelyAddDeepValue(restucturesdData, keysToCreate, value)
+
+        } else {
+            restucturesdData[key] = value;
+        }
+    })
+
+    return restucturesdData;
+}
